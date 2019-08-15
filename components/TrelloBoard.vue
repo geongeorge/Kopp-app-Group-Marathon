7ff6b79a84ae6d523c9c72cb4e4022bcb22c1365
Geon George <geongeorge98@gmail.com>
2019-08-15 15:47:30 +0530

    added axios

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class='embed-container'>
+            <iframe src="https://trello.com/b/vtuWFs7X.html" height="400" id="iHateRegex" frameborder="0"></iframe>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.embed-container {
+  position: relative;
+  height: auto;
+  overflow: hidden;
+  max-width: 100%;
+}
+.embed-container iframe,
+.embed-container object,
+.embed-container embed {
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-center text-5xl font-light text-gray-800"><span class="text-red-600 font-bold">No</span>Bullshit</h1>
+
+    <TrelloBoard :tid="userid"></TrelloBoard>
+
+    <div class="shadow">Hello</div>
+  </div>
+</template>
+
+<script>
+import TrelloBoard from '~/components/TrelloBoard.vue'
+
+export default {
+  components: {
+    TrelloBoard
+  },
+  data() {
+    return {
+      users: [
+        {
+          name: "Geon",
+          id: "vtuWFs7X"
+        },
+        {
+          name: "jithin",
+          id: "vtuWFs7X"
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+h1,h2,h3 {
+  font-family: 'Rubik', sans-serif;
+}
+</style>
